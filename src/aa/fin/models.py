3bf5260b2eddd1556d72f692f3202656c7410fb8
Sergey Chervyakov <wormer.new@gmail.com>
2025-04-17 16:11:36 -0300

    Add models

--- src/aa/fin/models.py
+++ src/aa/fin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    monthly_budget = models.DecimalField(decimal_places=2, max_digits=10)
+    daily_budget = models.DecimalField(decimal_places=2, max_digits=10)
+
+
+class Spend(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    datetime = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
